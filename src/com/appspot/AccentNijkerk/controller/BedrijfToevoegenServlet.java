34022d0b9f7e3b2f721ddb44e74413cc74c4ed50
Yassir Driuch <yassirdriuch@gmail.com>
2014-01-08 20:54:10 +0100

    [Werkend] JSPs en Servlets Gebruikers Aanmaken

---
+++ src/com/appspot/AccentNijkerk/controller/BedrijfToevoegenServlet.java
@@ -0,0 +1,55 @@
+package com.appspot.AccentNijkerk.controller;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import com.appspot.AccentNijkerk.model.*;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BedrijfToevoegenServlet extends HttpServlet {
+	private static final long serialVersionUID = -5060943264223383201L;
+	private static final Logger log = Logger.getLogger(LeerlingToevoegenServlet.class.getName());
+	GebruikerDao gebruikerDao = new GebruikerDaoOfyImpl();
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)	throws ServletException, IOException {
+		RequestDispatcher rd = req.getRequestDispatcher("bedrijfaanmaken.jsp");
+		String gebruikersnaam = req.getParameter("gebruikersnaam");
+		String wachtwoord = req.getParameter("wachtwoord");
+		String naam = req.getParameter("naam");
+		String adres = req.getParameter("adres");		
+		String email = req.getParameter("email");
+		String tel = req.getParameter("tel");
+		boolean intTest = true;
+		try{
+		int telInt = Integer.parseInt(tel);
+		}
+		catch(NumberFormatException e){
+			intTest = false;
+		}
+		
+		if(intTest == true){
+		
+			if(gebruikersnaam.equals("") || wachtwoord.equals("") || naam.equals("") || email.equals("") || adres.equals("") || tel.equals("")) {
+				req.setAttribute("msg", "<div class='nosucces'>Niet alle velden zijn ingevuld</div>");
+			} else {
+				Gebruiker g = (Gebruiker) new StageBedrijf(gebruikersnaam, wachtwoord, naam, adres, email, tel);
+				if(gebruikerDao.voegGebruikerToe(g)){
+				req.setAttribute("msg", "<div class='succes'>Bedrijf met account:<br />" + g + "<br />is toegevoegd</div>");
+				log.info( g + " is Toegevoegd");
+				}
+				else{
+					req.setAttribute("msg", "<div class='nosucces'>Bedrijf bestaat al</div>");
+				}
+			}
+		}
+		else {
+			req.setAttribute("msg", "<div class='nosucces'>Vul een geldige telefoonnummer in</div>");
+		}
+	rd.forward(req, resp);
+	}
+}
