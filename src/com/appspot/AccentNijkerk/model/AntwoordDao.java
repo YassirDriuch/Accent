55e295c2b35d4c828be5e6743de84acab50d24b9
Yassir Driuch <yassirdriuch@gmail.com>
2014-01-25 17:56:37 +0100

    Antwoord Dao en Impl

--- src/com/appspot/AccentNijkerk/model/AntwoordDao.java
+++ src/com/appspot/AccentNijkerk/model/AntwoordDao.java
@@ -1,9 +1,11 @@
 package com.appspot.AccentNijkerk.model;
 
+import java.util.ArrayList;
+
 public interface AntwoordDao {
 	public void voegAntwoordToe(Antwoord a);
 	public void updateAntwoord(Antwoord a);
 	public void verwijderAntwoord(Antwoord a);
-	public Antwoord getAntwoord(Antwoord);
-	
+	public Antwoord getAntwoord(Long id);
+	public ArrayList<Antwoord> getAntwoordenCP(Long competentieLijstId);
 }
--- src/com/appspot/AccentNijkerk/model/AntwoordDaoOfyImpl.java
+++ src/com/appspot/AccentNijkerk/model/AntwoordDaoOfyImpl.java
@@ -1,5 +1,36 @@
 package com.appspot.AccentNijkerk.model;
 
-public class AntwoordDaoOfyImpl {
+import java.util.ArrayList;
 
+import com.googlecode.objectify.Objectify;
+import com.googlecode.objectify.ObjectifyService;
+
+public class AntwoordDaoOfyImpl implements AntwoordDao{
+	Objectify ofy = ObjectifyService.begin();
+	
+	public AntwoordDaoOfyImpl(){
+		//Default constructor
+	}
+	
+	public void voegAntwoordToe(Antwoord a){
+		ofy.put(a);
+	}
+	
+	public void updateAntwoord(Antwoord a){
+		ofy.put(a);
+	}
+	
+	public void verwijderAntwoord(Antwoord a){
+		ofy.delete(a);
+	}
+
+	public Antwoord getAntwoord(Long id){
+		return ofy.find(Antwoord.class, id);
+	}
+	
+	public ArrayList getAntwoordenCP(Long competentieLijstId){
+		ArrayList<Antwoord> alleAntwoordenCP = new ArrayList<Antwoord>();
+		return null;
+	}
 }
+
