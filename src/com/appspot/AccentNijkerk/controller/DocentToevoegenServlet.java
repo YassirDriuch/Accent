e74e1597cb0f8d245f421326c17687b89edde112
mrJackz <marcellotjeh@live.nl>
2014-01-28 01:38:54 +0100

    Mail verzenden bij de rest van het creëren
    Vergeet niet om setFrom(!) te veranderen als je eht vanaf je gae
    applicatie draait. Deze mail heeft administratierechten nodig om te
    versturen, dus gebruik je eigen appspot email!

--- src/com/appspot/AccentNijkerk/controller/DocentToevoegenServlet.java
+++ src/com/appspot/AccentNijkerk/controller/DocentToevoegenServlet.java
@@ -1,10 +1,21 @@
 package com.appspot.AccentNijkerk.controller;
 
 import java.io.IOException;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 import com.appspot.AccentNijkerk.model.*;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -17,12 +28,16 @@ public class DocentToevoegenServlet extends HttpServlet {
 	GebruikerDao gebruikerDao = new GebruikerDaoOfyImpl();
 	
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)	throws ServletException, IOException {
-		RequestDispatcher rd = req.getRequestDispatcher("medewerker-toevoegen.jsp");
+		RequestDispatcher rd = req.getRequestDispatcher("docent-toevoegen.jsp");
 		String gebruikersnaam = req.getParameter("gebruikersnaam");
 		String wachtwoord = req.getParameter("wachtwoord");
 		String naam = req.getParameter("naam");
 		String adres = req.getParameter("adres");		
 		String email = req.getParameter("email");
+		
+		//Mail opzetten dmv properties te laden van je applicatie en een sessie.
+				Properties props = new Properties();
+				Session session = Session.getDefaultInstance(props, null); 
 		
 		if(gebruikersnaam.equals("") || wachtwoord.equals("") || naam.equals("") || email.equals("") || adres.equals("")) {
 			req.setAttribute("msg", "<div class='nosucces'>Niet alle velden zijn ingevuld</div>");
@@ -31,11 +46,54 @@ public class DocentToevoegenServlet extends HttpServlet {
 			if(gebruikerDao.voegGebruikerToe(g)){
 			req.setAttribute("msg", "<div class='succes'>Docent:<br />" + g + "<br />is toegevoegd</div>");
 			log.info( g + " is Toegevoegd");
+			//Mail verzenden
+			req.setAttribute("session", session);
+			sendMail(req, resp);
 			}
 			else{
 				req.setAttribute("msg", "<div class='nosucces'>Docent bestaat al</div>");
 			}
 		}
 	rd.forward(req, resp);
+	}
+	public void sendMail(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException{
+		String gebruikersnaam = req.getParameter("gebruikersnaam");
+		String wachtwoord = req.getParameter("wachtwoord");
+		String naam = req.getParameter("naam");
+		String email = req.getParameter("email");
+		Session session = (Session)req.getAttribute("session");
+		
+		//Onderwerp en bodypart
+		String subject = "Er is een account aangemaakt.";
+		String htmlBody = "Welkom bij de Accent Nijkerk app! <br></br> "
+				+ "Uw inloggegevens zijn: <br></br> Gebruikersnaam: " + " " + gebruikersnaam 
+				+ " <br> </br> Wachtwoord: " + " " + wachtwoord 
+				+ "<br></br> U kunt nu inloggen door: <a href='hu-marcel.appspot.com/index.jsp'>hier</a> te klikken." 
+				+ "<br></br> Dit is een automatisch verzonden email. Voor vragen neem contact op met: Helpdesk@AccentNijkerk.nl";
+		
+		try {
+			//Nieuwe message creëren
+		    Message msg = new MimeMessage(session);
+		    //Opsplitsen voor html gebruik
+		    Multipart mp = new MimeMultipart();
+			MimeBodyPart htmlPart = new MimeBodyPart();
+		    htmlPart.setContent(htmlBody, "text/html");
+		    mp.addBodyPart(htmlPart);
+			
+			//Van, voor en onderwerp setten
+		    msg.setFrom(new InternetAddress("mranimerater@live.nl", "Accent Nijkerk app"));
+		    msg.addRecipient(Message.RecipientType.TO,
+		    new InternetAddress(email, naam));
+		    msg.setSubject(subject);
+		    //Aan het nieuwe message de Multipart toevoegen als content
+		    msg.setContent(mp);
+		    Transport.send(msg);
+		    log.info("email is verzonden");
+
+		} catch (AddressException e) {
+		    log.info("Geen geldig email adres");
+		} catch (MessagingException e) {
+		    log.info(e.toString());
+		}
 	}
 }
--- src/com/appspot/AccentNijkerk/controller/StageBedrijfToevoegenServlet.java
+++ src/com/appspot/AccentNijkerk/controller/StageBedrijfToevoegenServlet.java
@@ -1,10 +1,21 @@
 package com.appspot.AccentNijkerk.controller;
 
 import java.io.IOException;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 import com.appspot.AccentNijkerk.model.*;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -25,6 +36,10 @@ public class StageBedrijfToevoegenServlet extends HttpServlet {
 		String email = req.getParameter("email");
 		String tel = req.getParameter("telnr");
 		
+		//Mail opzetten dmv properties te laden van je applicatie en een sessie.
+		Properties props = new Properties();
+		Session session = Session.getDefaultInstance(props, null); 
+		
 
 		if( gebruikersnaam.equals("") || wachtwoord.equals("") || naam.equals("") || email.equals("") || adres.equals("") || tel.equals("")) {
 			req.setAttribute("msg", "<div class='nosucces'>Niet alle velden zijn ingevuld</div>");
@@ -33,6 +48,8 @@ public class StageBedrijfToevoegenServlet extends HttpServlet {
 			if(gebruikerDao.voegGebruikerToe(g)){
 				req.setAttribute("msg", "<div class='succes'>Bedrijf met account:<br />" + g + "<br />is toegevoegd</div>");
 				log.info( g + " is Toegevoegd");
+				req.setAttribute("session", session);
+				sendMail(req, resp);
 			} else{
 				req.setAttribute("msg", "<div class='nosucces'>Bedrijf bestaat al</div>");
 			}
@@ -40,4 +57,45 @@ public class StageBedrijfToevoegenServlet extends HttpServlet {
 		rd.forward(req, resp);
 	} 
 	
+	public void sendMail(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException{
+		String gebruikersnaam = req.getParameter("gebruikersnaam");
+		String wachtwoord = req.getParameter("wachtwoord");
+		String naam = req.getParameter("naam");
+		String email = req.getParameter("email");
+		Session session = (Session)req.getAttribute("session");
+		
+		//Onderwerp en bodypart
+		String subject = "Er is een account aangemaakt.";
+		String htmlBody = "Welkom bij de Accent Nijkerk app! <br></br> "
+				+ "Uw inloggegevens zijn: <br></br> Gebruikersnaam: " + " " + gebruikersnaam 
+				+ " <br> </br> Wachtwoord: " + " " + wachtwoord 
+				+ "<br></br> U kunt nu inloggen door: <a href='hu-marcel.appspot.com/index.jsp'>hier</a> te klikken." 
+				+ "<br></br> Dit is een automatisch verzonden email. Voor vragen neem contact op met: Helpdesk@AccentNijkerk.nl";
+		
+		try {
+			//Nieuwe message creëren
+		    Message msg = new MimeMessage(session);
+		    //Opsplitsen voor html gebruik
+		    Multipart mp = new MimeMultipart();
+			MimeBodyPart htmlPart = new MimeBodyPart();
+		    htmlPart.setContent(htmlBody, "text/html");
+		    mp.addBodyPart(htmlPart);
+			
+			//Van, voor en onderwerp setten
+		    msg.setFrom(new InternetAddress("mranimerater@live.nl", "Accent Nijkerk app"));
+		    msg.addRecipient(Message.RecipientType.TO,
+		    new InternetAddress(email, naam));
+		    msg.setSubject(subject);
+		    //Aan het nieuwe message de Multipart toevoegen als content
+		    msg.setContent(mp);
+		    Transport.send(msg);
+		    log.info("email is verzonden");
+
+		} catch (AddressException e) {
+		    log.info("Geen geldig email adres");
+		} catch (MessagingException e) {
+		    log.info(e.toString());
+		}
+	
+}
 }
